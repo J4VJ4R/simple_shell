739682a4dbafdfb569ff0c50945d003d7445b98c
Javier Jaramillo <ingenierodesoftware73@gmail.com>
2020-04-07 22:36:22 +0000

    fix the error in mini_shell

--- Javi/mini_shell.c
+++ Javi/mini_shell.c
@@ -4,75 +4,76 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-
-int main(int argc, char **argv)
+#include <sys/stat.h>
+void conthand(int handle_signal);
+/**
+ * conthand - That handle the signal of ^C.
+ * @handle_signal: The signal that enters..
+ *
+ * Return: Void Funtion.
+ */
+void conthand(int handle_signal)
 {
-    //leer comando desde entrada
-    //primero necesitamos un puntero que guarde las lineas
-    //de entrada
+	if (handle_signal)
+		write(STDIN_FILENO, "\n#cisfun$ ", 10);
+}
+
+int main(int argc, char **argv, char *env[])
+{
     char *line = NULL;
-    size_t bufsize = 32;
-    int position = 0; //variable para recorrer los argumentos
-    //segundo un array de punteros donde guarde los
-    //argumentos
+    size_t bufsize = 32, read;
+    int position = 0;
     char **args = malloc(bufsize * sizeof(char *)); //asignamos memoria par no quedarnos cortos
     char *token;                                    //para guardar un solo argumento
-    //tercero un status se usa en wait(&status) para suspender
-    //la ejecución del proceso actual hasta que uno de sus hijos finaliza
     pid_t child_pid;
-    int status, i = 0;
+    int status;
     line = malloc(bufsize * sizeof(char));
     if (line == NULL)
     {
         perror("No hay espacio para la linea de entrada");
     }
-
     while (1)
     {
-        printf("cisfun$ ");
-
-        getline(&line, &bufsize, stdin);
-        //args = dividiendo_args();
-        // if (!args)
-        // {
-        //     fprintf(stderr, "no se pudo guardar argumentos");
-        // }
-        token = strtok(line, " \r\t\n\a");
-        while (token != NULL)
+        printf("1");
+        printf("#cisfun$ ");
+        
+        read = getline(&line, &bufsize, stdin);
+        //tests 
+        if (read < 0)
         {
-            args[position] = token;
-            position++;
-
-            token = strtok(NULL, " \r\t\n\a");
+            perror("Unexpected EOF or error:");
+            exit(EXIT_FAILURE);
         }
-        args[position] = NULL;
+
+        token = strtok(line, "\n");
+        position++;
+        void conthand(token);
+        // while (token != NULL)
+        // {
+        //     args[position] = token;
+        //     position++;
+
+        //     token = strtok(NULL, " \r\t\n\a");
+        // }
+        // args[position] = NULL;
         //args = args;
-        //status = ejecutar_args();
+
         child_pid = fork();
         if (child_pid == 0)
         {
-            //args[0] = line;
-            if (execvp(args[0], args) == -1)
+            args[0] = line;
+            if (execve(args[0], &argv[0], env[0]) == -1)
             {
-                perror("Error:");
+                perror("./shell: ");
             }
+            // printf("2");
             else
             {
                 wait(&status);
             }
         }
-
-        //printf("estas son tus entradas: %s %s", args[0], args[1]);
     }
-    //dividir los argumentos
-    while (args[position] != NULL)
-    {
-        free(args[i]);
-        position++;
-    }
-
     free(line);
     free(args);
-    //ejecutar los comandos analizados
     return (0);
 }
