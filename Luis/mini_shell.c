032d43045ac34c12045a50ee36f395e328031f3c
Lucho-ai <luchoalejomarin1999@gmail.com>
2020-04-10 17:14:08 +0000

    Fix in the mini shell

--- Luis/mini_shell.c
+++ Luis/mini_shell.c
@@ -1,5 +1,24 @@
 #include "shell.h"
 
+void _execute(char *args[])
+{
+    pid_t child_pid;
+    int status;
+
+    if ((child_pid = fork()) == 0)
+    {
+        if (execve(args[0], args, NULL) == -1)
+        {
+            perror("->Error:");
+            exit(127);
+        }
+    }
+    else
+    {
+        wait(&status);
+    }
+}
+
 char *_strdup(char *str)
 {
     int i, count;
