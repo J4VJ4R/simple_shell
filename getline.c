ecea2199d3f060d2342745df27756a439d8518bd
Lucho-ai <56923543+Lucho-ai@users.noreply.github.com>
2020-04-15 11:51:59 -0500

    Update getline.c

--- getline.c
+++ getline.c
@@ -14,3 +14,51 @@ int _getchar(void)
 	}
 	return (EOF);
 }
+/**
+ *_getline - Obtain a lin fron the standard input.
+ *@line: Pointer wher the line its gonna be stored.
+ *Return: Number of the line's characters.
+ */
+size_t _getline(char **line)
+{
+	size_t count = 0;
+	char *initial = *line;
+/*	char signal[1];*/
+
+	if (*line == NULL)
+	{
+		_printf("Line is NULL\n");
+		return (-1);
+	}
+	while (1)
+	{
+		fflush(stdout);/*In case that standard output is open.*/
+/*		_printf("args: %s\n", initial);*/
+		count = read(STDIN_FILENO, *line, 1024);
+		if (count < 1)
+		{
+			*line = initial;
+			return (-1);
+		}
+		else if (count == 1 && (*line)[count - 1] == '\n')
+		{
+			*line = initial;
+			if (string_len(initial) > 1)
+			{
+				return (string_len(initial));
+			}
+			return (0);
+		}
+		else if (count >= 1 && (*line)[count - 1] != '\n')
+		{
+			*line = *line + count;
+		}
+		else if (count >= 1)
+		{
+			(*line)[count - 1] = '\0';
+			*line = initial;
+			return (count);
+		}
+	}
+	return (count);
+}
